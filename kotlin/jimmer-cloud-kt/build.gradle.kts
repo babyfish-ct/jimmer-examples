d3c763e71ef3a1c52cb4249b0b23caefbaefeed6
babyfish-ct <babyfish.ct@gmail.com>
2024-02-23 17:31:34 +0800

    0.8.100

--- kotlin/jimmer-cloud-kt/build.gradle.kts
+++ kotlin/jimmer-cloud-kt/build.gradle.kts
@@ -1,4 +1,4 @@
-val jimmerVersion by extra { "0.8.98" }
+val jimmerVersion by extra { "0.8.100" }
 val springCloudVersion by extra { "2021.0.3" }
 
 allprojects {
--- kotlin/save-command-kt/build.gradle.kts
+++ kotlin/save-command-kt/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.21-1.0.15"
 }
 
-val jimmerVersion = "0.8.98"
+val jimmerVersion = "0.8.100"
 
 group = "org.babyfish.jimmer.example.save"
 version = jimmerVersion
