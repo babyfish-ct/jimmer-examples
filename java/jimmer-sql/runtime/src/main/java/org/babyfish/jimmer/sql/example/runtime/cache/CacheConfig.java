7da7ad00c5ece4bd53358bac8e74a758fbb640e7
babyfish-ct <babyfish.ct@gmail.com>
2024-07-22 04:36:37 +0800

    remove unnecessary code

--- java/jimmer-sql/runtime/src/main/java/org/babyfish/jimmer/sql/example/runtime/cache/CacheConfig.java
+++ java/jimmer-sql/runtime/src/main/java/org/babyfish/jimmer/sql/example/runtime/cache/CacheConfig.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.Duration;
 import java.util.List;
@@ -23,7 +22,6 @@ import java.util.List;
 // -----------------------------
 @ConditionalOnProperty("spring.redis.host")
 @Configuration
-@EnableScheduling
 public class CacheConfig {
 
     @Bean
