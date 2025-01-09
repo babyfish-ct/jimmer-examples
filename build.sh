ba8decca47df4addb8d02e1cc0ae8750b04fa080
babyfish-ct <babyfish.ct@gmail.com>
2025-01-09 22:03:04 +0800

    build.sh

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+root_dir=$PWD
+dirs=(\
+    "java/jimmer-core" "java/jimmer-sql" "java/jimmer-sql-graphql" \
+    "java/jimmer-cloud" "java/save-command" "kotlin/jimmer-core-kt" \
+    "kotlin/jimmer-sql-kt" "kotlin/jimmer-sql-graphql-kt" \
+    "kotlin/jimmer-cloud-kt" "kotlin/save-command-kt"\
+)
+for dir in "${dirs[@]}"; do
+    echo --------
+    echo $dir
+    echo --------
+    cd "$root_dir/$dir"
+    if [[ $dir == java/* ]]; then
+        echo "maven..."
+        ./mvnw clean; ./mvnw install	
+    fi
+    echo "gradle ..."
+    ./gradlew clean; ./gradlew build
+    cd "$root_dir"
+done
