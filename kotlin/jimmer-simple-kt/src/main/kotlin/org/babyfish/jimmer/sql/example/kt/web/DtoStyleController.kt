24ef9f29aa9b09f0fdcd28ccb415e14167c1ab86
babyfish-ct <babyfish.ct@gmail.com>
2025-02-17 22:57:34 +0800

    Add documentation comment

--- kotlin/jimmer-simple-kt/src/main/kotlin/org/babyfish/jimmer/sql/example/kt/web/DtoStyleController.kt
+++ kotlin/jimmer-simple-kt/src/main/kotlin/org/babyfish/jimmer/sql/example/kt/web/DtoStyleController.kt
@@ -1,13 +1,10 @@
 package org.babyfish.jimmer.sql.example.kt.web
 
-import org.babyfish.jimmer.sql.example.kt.model.Book
-import org.babyfish.jimmer.sql.example.kt.model.TreeNode
+import org.babyfish.jimmer.sql.example.kt.model.*
 import org.babyfish.jimmer.sql.example.kt.model.dto.ComplexBookView
 import org.babyfish.jimmer.sql.example.kt.model.dto.RecursiveTreeNodeView
 import org.babyfish.jimmer.sql.example.kt.model.dto.SimpleBookView
 import org.babyfish.jimmer.sql.example.kt.model.dto.SimpleTreeNodeView
-import org.babyfish.jimmer.sql.example.kt.model.name
-import org.babyfish.jimmer.sql.example.kt.model.parentId
 import org.babyfish.jimmer.sql.kt.KSqlClient
 import org.babyfish.jimmer.sql.kt.ast.expression.`eq?`
 import org.babyfish.jimmer.sql.kt.ast.expression.isNull
@@ -24,6 +21,17 @@ class DtoStyleController(
     private val sqlClient: KSqlClient
 ) {
 
+    /**
+     * Find simple book objects.
+     *
+     * If this parameter is specified, only books with the specified name will be queried.
+     *
+     * @param name Optional query parameter,
+     * `null` and `""` will be ignored
+     *
+     * @return A list of Book objects, each of which
+     * has only `name` and `edition` properties
+     */
     @GetMapping("/books/simple")
     fun findSimpleBooks(
         @RequestParam(required = false) name: String?
@@ -33,6 +41,19 @@ class DtoStyleController(
             select(table.fetch(SimpleBookView::class))
         }
 
+    /**
+     * Find complex book objects.
+     *
+     * If this parameter is specified, only books with the specified name will be queried.
+     *
+     * @param name Optional query parameter,
+     * `null` and `""` will be ignored
+     *
+     * @return A list of Book objects, each of which
+     * has all scalar properties, associated
+     * [BookStore] and associated
+     * [Author] objects.
+     */
     @GetMapping("/books/complex")
     fun findComplexBooks(
         @RequestParam(required = false) name: String?
@@ -42,6 +63,12 @@ class DtoStyleController(
             select(table.fetch(ComplexBookView::class))
         }
 
+    /**
+     * Query root tree nodes, that is, nodes whose parentId is null.
+     *
+     * @return All list of root tree nodes, each of which
+     * has only `id` and `name` properties
+     */
     @GetMapping("/rootTreeNodes/simple")
     fun findSimpleTreeNodes(
     ): List<SimpleTreeNodeView> =
@@ -50,6 +77,15 @@ class DtoStyleController(
             select(table.fetch(SimpleTreeNodeView::class))
         }
 
+    /**
+     * Query root tree nodes, that is, nodes whose parentId is null.
+     *
+     * @return All list of root tree nodes, each of which
+     *         has all scalar properties, associated child objects.
+     *         If a child node has deeper child nodes,
+     *         it will be recursively associated until
+     *         there are no deeper child nodes.
+     */
     @GetMapping("/rootTreeNodes/recursive")
     fun findRecursiveTreeNodes(
     ): List<RecursiveTreeNodeView> =
