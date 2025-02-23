e7d117b38809d3d2aa950bcf4ef0009983750f4e
babyfish-ct <babyfish.ct@gmail.com>
2025-02-24 01:08:23 +0800

    0.9.59

--- kotlin/jimmer-cloud-kt/build.gradle.kts
+++ kotlin/jimmer-cloud-kt/build.gradle.kts
@@ -1,4 +1,4 @@
-val jimmerVersion by extra { "0.9.58" }
+val jimmerVersion by extra { "0.9.59" }
 
 // Jimmer remote associations require ANTLR
 val antlrVersion by extra { "4.13.2"}
--- kotlin/save-command-kt/build.gradle.kts
+++ kotlin/save-command-kt/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.21-1.0.15"
 }
 
-val jimmerVersion = "0.9.58"
+val jimmerVersion = "0.9.59"
 
 group = "org.babyfish.jimmer.example.save"
 version = jimmerVersion
