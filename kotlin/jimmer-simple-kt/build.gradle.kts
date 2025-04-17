2b87330588b70a4574909c84edf0a4a0196d57d8
babyfish-ct <babyfish.ct@gmail.com>
2025-04-18 02:42:47 +0800

    0.9.75

--- kotlin/jimmer-simple-kt/build.gradle.kts
+++ kotlin/jimmer-simple-kt/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.12.RELEASE"
 }
 
-val jimmerVersion = "0.9.74"
+val jimmerVersion = "0.9.75"
 group = "org.babyfish.jimmer.example.kt"
 version = jimmerVersion
 
--- kotlin/jimmer-sql-kt/build.gradle.kts
+++ kotlin/jimmer-sql-kt/build.gradle.kts
@@ -1,4 +1,4 @@
-val jimmerVersion by extra { "0.9.74" }
+val jimmerVersion by extra { "0.9.75" }
 val springBootVersion by extra { "2.7.0" }
 
 allprojects {
