d1abf2873d95d20f1bfd52d765dc6f3e6d9184b1
babyfish-ct <babyfish.ct@gmail.com>
2025-04-26 04:45:07 +0800

    0.9.77

--- kotlin/jimmer-core-kt/build.gradle.kts
+++ kotlin/jimmer-core-kt/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.21-1.0.15"
 }
 
-val jimmerVersion = "0.9.76"
+val jimmerVersion = "0.9.77"
 
 group = "org.babyfish.jimmer.example.kt"
 version = jimmerVersion
