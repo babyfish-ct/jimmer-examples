5a107d3f0460fed9126ce6b4042210c9a09e877d
babyfish-ct <babyfish.ct@gmail.com>
2025-05-13 09:26:50 +0800

    0.9.83

--- kotlin/jimmer-cloud-kt/build.gradle.kts
+++ kotlin/jimmer-cloud-kt/build.gradle.kts
@@ -1,4 +1,4 @@
-val jimmerVersion by extra { "0.9.82" }
+val jimmerVersion by extra { "0.9.83" }
 
 // Jimmer remote associations require ANTLR
 val antlrVersion by extra { "4.13.2"}
--- kotlin/jimmer-sql-graphql-kt/build.gradle.kts
+++ kotlin/jimmer-sql-graphql-kt/build.gradle.kts
@@ -1,4 +1,4 @@
-val jimmerVersion by extra { "0.9.82" }
+val jimmerVersion by extra { "0.9.83" }
 val springBootVersion by extra { "2.7.0" }
 
 allprojects {
