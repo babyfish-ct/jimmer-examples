4d0dfbee1e9cb2cdfa91e2c30dece8e8620d0f7b
babyfish-ct <babyfish.ct@gmail.com>
2025-06-15 04:13:01 +0800

    0.9.91

--- kotlin/save-command-kt/build.gradle.kts
+++ kotlin/save-command-kt/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.1.20-2.0.0"
 }
 
-val jimmerVersion = "0.9.90"
+val jimmerVersion = "0.9.91"
 
 group = "org.babyfish.jimmer.example.save"
 version = jimmerVersion
