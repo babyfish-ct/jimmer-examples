802e03dacdca4eef071486b508870fbb52556517
babyfish-ct <babyfish.ct@gmail.com>
2025-08-13 01:36:25 +0800

    0.9.106

--- kotlin/jimmer-core-kt/build.gradle.kts
+++ kotlin/jimmer-core-kt/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.1.20-2.0.0"
 }
 
-val jimmerVersion = "0.9.105"
+val jimmerVersion = "0.9.106"
 
 group = "org.babyfish.jimmer.example.kt"
 version = jimmerVersion
--- kotlin/jimmer-sql-graphql-kt/build.gradle.kts
+++ kotlin/jimmer-sql-graphql-kt/build.gradle.kts
@@ -1,4 +1,4 @@
-val jimmerVersion by extra { "0.9.105" }
+val jimmerVersion by extra { "0.9.106" }
 val springBootVersion by extra { "2.7.0" }
 
 allprojects {
